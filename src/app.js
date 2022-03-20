bad1e767fc4988c9a12d601d6f3855fca7ef86f3
AnnalisaN8 <88476122+AnnalisaN8@users.noreply.github.com>
2022-03-20 17:20:26 +0100

    Added API call for daily forecast

--- src/app.js
+++ src/app.js
@@ -53,7 +53,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function displayTemp(response) {
+function getForecast(coordinates) {
+  let apiKey = "caacaca526de1415f6c61fb8c2edfa11";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -75,12 +82,14 @@ function displayTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
   let apiKey = "caacaca526de1415f6c61fb8c2edfa11";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemp);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
@@ -118,4 +127,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Rome");
-displayForecast();
